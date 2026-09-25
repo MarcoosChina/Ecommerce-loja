a1d7220612725d7690b23806d25e35d1cf9933d9
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-09-24 23:25:50 -0300

    feat: cria ProductDetail com rota dinâmica, integra ao ProductCard e trata produto inexistente

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Product } from '../../models/product';
 import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css'
 })
